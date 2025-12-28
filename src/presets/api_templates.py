4dce81ce7559e0f11f6a3ed023ea0b75885f3842
Кот просто Кот <iiiiiiiiiilllll@yandex.ru>
2025-12-28 14:56:50 +0300

    fix: убрал amazon/nova-2-lite-v1:free из default_model
    - Убрана amazon/nova-2-lite-v1:free из шаблона OpenRouter, так как она перестала функционировать. Вместо неё прописал - z-ai/glm-4.5-air:free

--- src/presets/api_templates.py
+++ src/presets/api_templates.py
@@ -27,10 +27,9 @@ API_TEMPLATES_DATA = [
         "name": "OpenRouter",
         "pricing": "mixed",
         "url": "https://openrouter.ai/api/v1/chat/completions",
-        "default_model": "amazon/nova-2-lite-v1:free",
+        "default_model": "z-ai/glm-4.5-air:free",
         "known_models": [
             "mistralai/devstral-2512:free",
-            "z-ai/glm-4.5-air:free",
             "arcee-ai/trinity-mini:free",
             "nex-agi/deepseek-v3.1-nex-n1:free"
         ],
